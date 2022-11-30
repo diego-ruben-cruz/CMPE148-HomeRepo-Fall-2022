b27be9dc07e14b9fc0984e35f90542cfae5a87d5
Diego Cruz <diego.rubencruz@gmail.com>
2022-11-29 23:49:41 -0800

    Almost finished, needs logout

--- hw2-Python-Flask-Server-JWT/flaskJWT.py
+++ hw2-Python-Flask-Server-JWT/flaskJWT.py
@@ -15,42 +15,80 @@
 	Homework: HW2
 	Date: 30 November 2022
 """
+
 # imports the used assets
-from hmac import compare_digest
+from flask import Flask, request, jsonify, make_response, request, render_template, session, redirect
+import jwt
+from datetime import datetime, timedelta
+from functools import wraps
 
-from flask import Flask
-from flask import jsonify
-from flask import request
-from flask_sqlalchemy import SQLAlchemy
 
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import current_user
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
-# Created an instance of a Flask and used __name__ as a shortcut
-app = Flask(__name__)
+# Created an instance of a Flask and used __name__ as a shortcut, template folder is set as a default dependency
+app = Flask(__name__,template_folder='templates')
+app.config['SECRET_KEY'] = '83566d66ee8349d4b43d31d276207c6d'
 
-app.config["JWT_SECRET_KEY"] = "flaskJWT_Python"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+def token_required(func):
+    # decorator factory which invokes update_wrapper() method and passes decorated function as an argument
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'Alert!': 'Token is missing!'}), 401
 
-jwt = JWTManager(app)
-db = SQLAlchemy(app)
+        try:
 
-# route() tells flask what URL triggers our function
-@app.route("/")
-def homepage():
-    return "<p>homepage</p>"
-    # The function returns a basic "homepage" message.
-    # You can make this more complicated by adding other routes for sub URLs.
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        # You can use the JWT errors in exception
+        # except jwt.InvalidTokenError:
+        #     return 'Invalid token. Please log in again.'
+        except:
+            return jsonify({'Message': 'Invalid token'}), 403
+        return func(*args, **kwargs)
+    return decorated
 
-@app.route("/hello-world")
-def helloworld():
-    return "<p>Hello, World!</p>"
-    # The function returns a basic "Hello, World!" message.
-    # You can make this more complicated by adding other routes for sub URLs.
 
-@app.route("/hello-world/subURL-example")
-def helloworldsub():
-	return "<p>Hello, subURL!</p>"
-	# The function returns a basic "Hello, subURL!" message.
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return 'logged in currently'
+
+# Just to show you that a public route is available for everyone
+
+
+@app.route('/public')
+def public():
+    return 'For Public'
+
+# auth only if you copy your token and paste it after /auth?query=XXXXXYour TokenXXXXX
+# Hit enter and you will get the message below.
+
+
+@app.route('/auth')
+@token_required
+def auth():
+    return 'JWT is verified. Welcome to your dashboard !'
+
+# Login page
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['username'] and request.form['password'] == '123456':
+        session['logged_in'] = True
+
+        token = jwt.encode({
+            'user': request.form['username'],
+            'expiration': str(datetime.utcnow() + timedelta(seconds=60))
+        },
+            app.config['SECRET_KEY'])
+        return jsonify({'token': token})
+    else:
+        return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm: "Authentication Failed!'})
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session['name'] = None
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- hw2-Python-Flask-Server-JWT/hw2-setup.md
+++ hw2-Python-Flask-Server-JWT/hw2-setup.md
@@ -1,5 +1,7 @@
 # How to set up and run a flask project with JWT
+
 ## Installation
+
     Install Python @ <https://https://www.python.org/downloads/>
 
     Create a virtual environment in cmdline or powershell by navigating to intended folder
@@ -9,14 +11,19 @@
     Activate the virtual environment by using the following cmd or powershell input
     `venv\Scripts\activate`
 
-### If the above fails, use the following powershell command
+    Install the jwt command line interface using the following npm command in a separate powershell/cmd instance
+    `npm install -g jwt-cli`
+
+### If virtual environment activation fails, use the following powershell command
+
     `Set-ExecutionPolicy Unrestricted -Scope Process`
 
-    After activating the virtual environment, use the following command to install flask
-    and other assets.
-    `pip install flask flask_jwt_extended flask_sqlalchemy`
+### After activating the virtual environment, use the following command to install flask and other assets.
+
+    `pip install flask flask_jwt_extended flask_sqlalchemy PyJWT`
 
 ## To Run
+
     Inside of your virtual environment, use the basic commands:
     `flask --app flaskJWT run`
     OR
